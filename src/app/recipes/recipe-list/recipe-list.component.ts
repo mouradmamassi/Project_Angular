4a2ddd7c2029ee30a162edb391543a81161a0a70
Mourad MAMASSI <mourad.mamassi@gmail.com>
2019-11-16 23:37:40 +0100

    chane recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {RecipeModel} from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,13 +8,20 @@ import {RecipeModel} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<RecipeModel>();
   recipes: RecipeModel[] = [
     new RecipeModel('A Test Recipe', 'this is sipmly description',
       'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_2560,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
+    new RecipeModel('Another Test Recipe', 'this is another sipmly description',
+    'https://files.meilleurduchef.com/mdc/photo/recette/tajine-agneau/tajine-agneau-640.jpg'),
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  OnRecipeSelected(recipe: RecipeModel){
+     this.recipeWasSelected.emit(recipe);
   }
 
 }
