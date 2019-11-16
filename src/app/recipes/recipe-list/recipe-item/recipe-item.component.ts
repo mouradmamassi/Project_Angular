8a87e70da2eb78db596fd0595a77281b4cbf04f0
Mourad MAMASSI <mourad.mamassi@gmail.com>
2019-11-16 23:37:40 +0100

    chane recipe list

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,9 +8,14 @@ import { Component, OnInit, Input } from '@angular/core';
 export class RecipeItemComponent implements OnInit {
 
   @Input() recipeItem: RecipeItemComponent;
+  @Output() recipeSelected = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected(){
+    this.recipeSelected.emit();
   }
 
 }
